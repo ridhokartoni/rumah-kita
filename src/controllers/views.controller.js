c8a41a7e39be2ea37b521541be6d47287c8092b4
rafusr <andpuji27@gmail.com>
2022-01-09 21:03:53 +0700

    [FIX] Home Page

--- src/controllers/views.controller.js
+++ src/controllers/views.controller.js
@@ -1,4 +1,4 @@
-
+const formatterDate = require('../utilities/formatterDate');
 
 exports.home = async (req, res) => {
     if(req.query.token){
@@ -6,12 +6,281 @@ exports.home = async (req, res) => {
         res.render('../views/pages/home_page.ejs', {})
     }
     let data = {
-        category : ["Mental Issues", "Lifestyle"],
-        isLogged : false
+        user: {
+            name: 'Alma Lawson',
+            email: 'alma.lawson@example.com'
+        },
+        navItem: [
+            {
+                isActive: 'active',
+                name: 'Terkini',
+                url: '/'
+            },
+            {
+                isActive: '',
+                name: 'Mental Issues',
+                url: '/mentalissue'
+            },
+            {
+                isActive: '',
+                name: 'Gaya Hidup',
+                url: '/lifestyle'
+            },
+            {
+                isActive: '',
+                name: 'Kasus',
+                url: '/case'
+            },
+            {
+                isActive: '',
+                name: 'Tersimpan',
+                url: '/saved'
+            },
+        ],
+        dateNow: formatterDate.currentDate,
+        articles:
+        {
+            newest: [
+
+            ],
+
+            specials: [
+
+            ],
+
+            cases: [
+
+            ],
+
+            mostPopular: [
+
+            ],
+
+            mostSaved: [
+
+            ]
+
+        },
+        isLogged: false
     };
     res.render('../views/pages/home_page.ejs', {
-        category : ["Mental Issues", "Lifestyle"],
-        isLogged : false
+        data: data
+    });
+}
+
+exports.mentalissues = async (req, res) => {
+    let data = {
+        user: {
+            name: 'Alma Lawson',
+            email: 'alma.lawson@example.com'
+        },
+        navItem: [
+            {
+                isActive: '',
+                name: 'Terkini',
+                url: '/'
+            },
+            {
+                isActive: 'active',
+                name: 'Mental Issues',
+                url: '/mentalissues'
+            },
+            {
+                isActive: '',
+                name: 'Gaya Hidup',
+                url: '/lifestyle'
+            },
+            {
+                isActive: '',
+                name: 'Kasus',
+                url: '/case'
+            },
+            {
+                isActive: '',
+                name: 'Tersimpan',
+                url: '/saved'
+            },
+        ],
+        dateNow: formatterDate.currentDate,
+        articles:
+        {
+            specials: [
+
+            ],
+
+            cases: [
+
+            ],
+
+            mostPopular: [
+
+            ],
+        },
+        isLogged: false
+    };
+    res.render('../views/pages/categories/mental_issues.ejs', {
+        data: data
+    });
+}
+
+exports.lifestyle = async (req, res) => {
+    let data = {
+        user: {
+            name: 'Alma Lawson',
+            email: 'alma.lawson@example.com'
+        },
+        navItem: [
+            {
+                isActive: '',
+                name: 'Terkini',
+                url: '/'
+            },
+            {
+                isActive: '',
+                name: 'Mental Issues',
+                url: '/mentalissues'
+            },
+            {
+                isActive: 'active',
+                name: 'Gaya Hidup',
+                url: '/lifestyle'
+            },
+            {
+                isActive: '',
+                name: 'Kasus',
+                url: '/case'
+            },
+            {
+                isActive: '',
+                name: 'Tersimpan',
+                url: '/saved'
+            },
+        ],
+        dateNow: formatterDate.currentDate,
+        articles:
+        {
+            specials: [
+
+            ],
+
+            cases: [
+
+            ],
+
+            mostPopular: [
+
+            ],
+        },
+        isLogged: false
+    };
+    res.render('../views/pages/categories/gaya_hidup.ejs', {
+        data: data
+    });
+}
+
+exports.case = async (req, res) => {
+    let data = {
+        user: {
+            name: 'Alma Lawson',
+            email: 'alma.lawson@example.com'
+        },
+        navItem: [
+            {
+                isActive: '',
+                name: 'Terkini',
+                url: '/'
+            },
+            {
+                isActive: '',
+                name: 'Mental Issues',
+                url: '/mentalissues'
+            },
+            {
+                isActive: '',
+                name: 'Gaya Hidup',
+                url: '/lifestyle'
+            },
+            {
+                isActive: 'active',
+                name: 'Kasus',
+                url: '/case'
+            },
+            {
+                isActive: '',
+                name: 'Tersimpan',
+                url: '/saved'
+            },
+        ],
+        dateNow: formatterDate.currentDate,
+        articles:
+        {
+            specials: [
+
+            ],
+
+            cases: [
+
+            ],
+
+            mostPopular: [
+
+            ],
+        },
+        isLogged: false
+    };
+    res.render('../views/pages/categories/kasus.ejs', {
+        data: data
+    });
+}
+
+exports.saved = async (req, res) => {
+    let data = {
+        user: {
+            name: 'Alma Lawson',
+            email: 'alma.lawson@example.com'
+        },
+        navItem: [
+            {
+                isActive: '',
+                name: 'Terkini',
+                url: '/'
+            },
+            {
+                isActive: '',
+                name: 'Mental Issues',
+                url: '/mentalissues'
+            },
+            {
+                isActive: '',
+                name: 'Gaya Hidup',
+                url: '/lifestyle'
+            },
+            {
+                isActive: '',
+                name: 'Kasus',
+                url: '/case'
+            },
+            {
+                isActive: 'active',
+                name: 'Tersimpan',
+                url: '/saved'
+            },
+        ],
+        dateNow: currentDate,
+        articles:
+        {
+            newest: [
+
+            ],
+
+            saved: [
+
+            ],
+        },
+        isLogged: false
+    };
+    res.render('../views/pages/saved_page.ejs', {
+        data: data
     });
 }
 
