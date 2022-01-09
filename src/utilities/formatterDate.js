9c28c863f7d4dfdafca5ba37718fdab5cb272141
momyuke <triprasetyat@gmail.com>
2022-01-09 13:52:53 +0700

    [ADD] Utils Dateformatter

--- src/utilities/formatterDate.js
+++ src/utilities/formatterDate.js
@@ -1,3 +1,9 @@
-function formatterDate(date){
-    
+const moment = require('moment');
+
+exports.formatterDate = function() {
+    moment.locale('id');
+    const dateLocalWIB = moment().format('LLLL').split(" ");
+    return dateLocalWIB[0] + " " + dateLocalWIB[1] + " "+  dateLocalWIB[2]+ " " + dateLocalWIB[3]
 }
+
+
