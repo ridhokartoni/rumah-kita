6e96029f16b61c06376b6597250bb5e2aa3e6259
momyuke <triprasetyat@gmail.com>
2022-01-14 05:20:14 +0700

    [ADD] Service for articles

---
+++ src/services/articles.services.js
@@ -0,0 +1,69 @@
+const Article = require('../model/article.model');
+const { Op } = require('sequelize')
+
+exports.getSomeArticle = async (limit) => {
+    let result;
+    try {
+        result = await Article.findAll({
+            limit: limit
+        });
+
+        return result
+    } catch (error) {
+        throw new Error(error.message)
+    }
+}
+
+exports.searchArticle = async (keyword, limit) => {
+    let result;
+    try {
+        result = await Article.findAll({
+            where: {
+                [Op.or]: [
+                    {
+                        title: {
+                            [Op.contains]: keyword
+                        }
+                    },
+                    {
+                        content: {
+                            [Op.contains]: keyword
+                        }
+                    }
+                ]
+            },
+            limit : limit
+        })
+
+        return result
+    } catch (error) {
+        throw new Error(error.message)
+    }
+}
+
+
+exports.articleByCategory = async (categoryId, limit) => {
+    let result;
+    try {
+        result = await Article.findAll({
+            where : {
+                categoryId : categoryId
+            },
+            limit : limit
+        })
+    } catch (error) {
+        throw new Error(error.message)
+    }
+}
+
+
+// exports.mostSavedArticles = async (limit) => {
+//     let result;
+//     try {
+//         result = await Article.findAll({
+
+//         })
+//     } catch (error) {
+//         throw new Error(error.message)
+//     }
+// }
