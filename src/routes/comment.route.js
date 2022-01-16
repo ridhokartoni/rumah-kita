891758ace996bc946abf5b1c70008735f9a06270
momyuke <triprasetyat@gmail.com>
2022-01-16 16:56:36 +0700

    [fix] bug

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const CommentController = require('../controllers/comment.controller');
+const userAuthentication = require('../middleware/userAuthenticationToken');
 
-
+router.use(userAuthentication);
 router.get('/:articleId', CommentController.findAll);
 router.post('/create', CommentController.create);
 
